24e4770e18f287ec5e8f548286ddafbf09cd0818
tanvirahmod <tanvir.ahmod@gmail.com>
2023-10-18 06:46:02 +0600

    added title to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import SearchBar from "@/components/search/SearchBar";
 import HomeIntros from "@/components/home-intros/HomeIntros";
 
 export const metadata = {
-  title: "meaninginhindi.wiki | English to Hindi Online Dictionary.",
+  title: "English to Hindi Online Dictionary | अंग्रेज़ी हिन्दी शब्दकोश - MeaningInHindi",
   description: "We're all digging deep into words to help you understand, what they mean in Hindi. We're not just giving you simple definitions – we're like worddetectives, uncovering all the interesting details and translation in Hindi about each word.",
   alternates: {
     canonical: `https://meaninginhindi.wiki`
